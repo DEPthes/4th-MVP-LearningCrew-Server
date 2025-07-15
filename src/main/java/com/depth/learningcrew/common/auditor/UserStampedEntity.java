8917f0504fd9e074ee0707e9cdf8985df7e1adbb
천명현 <cmh1448@naver.com>
2025-07-16 01:38:40 +0900

    feat: 애플리케이션 및 감사 엔티티 클래스 추가
    - LearnitApplication 클래스를 생성하여 Spring Boot 애플리케이션의 진입점 설정
    - TimeStampedEntity 및 UserStampedEntity 클래스를 추가하여 생성 및 수정 날짜를 자동으로 관리하는 감사 기능 구현
    - LearnitApplicationTests 클래스를 추가하여 애플리케이션 컨텍스트 로드 테스트 추가

---
+++ src/main/java/com/depth/learningcrew/common/auditor/UserStampedEntity.java
@@ -0,0 +1,38 @@
+package com.depth.learningcrew.common.auditor;
+
+
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.SuperBuilder;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@MappedSuperclass
+@SuperBuilder
+@EntityListeners(AuditingEntityListener.class)
+@NoArgsConstructor
+@AllArgsConstructor
+public abstract class UserStampedEntity {
+    @CreatedDate
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    private LocalDateTime lastModifiedAt;
+
+//    @CreatedBy
+//    @ManyToOne(fetch = FetchType.LAZY)
+//    private User createdBy;
+//
+//    @LastModifiedBy
+//    @ManyToOne(fetch = FetchType.LAZY)
+//    private User lastModifiedBy;
+}
