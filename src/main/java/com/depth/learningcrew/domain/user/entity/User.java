9858fca354437aed8b7d3b0e24d49a7b444be04a
Hooby <hooby11105@naver.com>
2025-07-18 11:16:19 +0900

    refactor: User Entity->필드 추가 및 제약조건 설정

--- src/main/java/com/depth/learningcrew/domain/user/entity/User.java
+++ src/main/java/com/depth/learningcrew/domain/user/entity/User.java
@@ -1,22 +1,32 @@
 package com.depth.learningcrew.domain.user.entity;
 
 import com.depth.learningcrew.common.auditor.TimeStampedEntity;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.*;
+
+import java.time.LocalDate;
 
 @Entity
 @AllArgsConstructor
 @NoArgsConstructor
 @Getter
 @Setter
-@Table(name = "UserAccount")
+@Table(name = "USER")
 public class User extends TimeStampedEntity {
     @Id @Setter(AccessLevel.NONE)
+    @Column(name= "id", length = 50)
     private String email;
 
+    @Column(nullable=false, length = 50)
     private String password;
 
-    private String name;
+    @Column(nullable=false, length = 30)
+    private String nickname;
+
+    @Column(nullable=false)
+    private LocalDate birthday;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable=false, length = 20)
+    private Gender gender;
 }
