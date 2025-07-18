aeb1a7a580935bf9dfe612513bc2735edacf7603
Hooby <hooby11105@naver.com>
2025-07-18 13:04:28 +0900

    refactor: AuthDetails에 getAuthorities() 추상메서드 등록

--- src/main/java/com/depth/learningcrew/system/security/model/AuthDetails.java
+++ src/main/java/com/depth/learningcrew/system/security/model/AuthDetails.java
@@ -1,6 +1,9 @@
 package com.depth.learningcrew.system.security.model;
 
 import org.springframework.security.core.AuthenticatedPrincipal;
+import org.springframework.security.core.GrantedAuthority;
+
+import java.util.Collection;
 
 public abstract class AuthDetails implements AuthenticatedPrincipal {
     public abstract String getKey();
@@ -8,4 +11,6 @@ public abstract class AuthDetails implements AuthenticatedPrincipal {
     public String getName() {
         return this.getKey();
     }
+
+    public abstract Collection<? extends GrantedAuthority> getAuthorities();
 }
