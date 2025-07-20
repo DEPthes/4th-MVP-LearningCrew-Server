27c88538c96380c039001ffeeb65cadf712d35e7
Hooby <hooby11105@naver.com>
2025-07-20 10:12:17 +0900

    comment: JwtAuthenticationFilter의 논의점에 관한 주석 해제

--- src/main/java/com/depth/learningcrew/system/security/filter/JwtAuthenticationFilter.java
+++ src/main/java/com/depth/learningcrew/system/security/filter/JwtAuthenticationFilter.java
@@ -50,12 +50,6 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
                 if(userDetails.isEmpty()) {throw new JwtInvalidTokenException();}
 
-                /* 추후 리팩토링 고려 지점 -> JwtTokenProvider에서 getAuthentication을 하는게 가독성 측면에선 깔끔
-                *  Resolver와 Provider의 역할을 분리하는게 맞는지 고민 필요
-                *  SRP에 의하면 분리하는게 맞긴한데, 가독성까지 챙겨가자니 DI가 너무 가중됨
-                *  만약 Provider에서 Validation과 getAuthentication을 모두 처리한다면 8줄을 날릴 수 있음
-                *  대신 Provider가 너무 많은 책임을 가지게 됨
-                */
                 SecurityContextHolder.getContext()
                         .setAuthentication(
                                 new UsernamePasswordAuthenticationToken(
