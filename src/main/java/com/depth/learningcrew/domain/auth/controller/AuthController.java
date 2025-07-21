40369e07a3b590fccdd125ddf7549b79bbef6251
Hooby <hooby11105@naver.com>
2025-07-22 08:52:02 +0900

    feat: 내 정보 조회 API 정의

--- src/main/java/com/depth/learningcrew/domain/auth/controller/AuthController.java
+++ src/main/java/com/depth/learningcrew/domain/auth/controller/AuthController.java
@@ -4,11 +4,14 @@ import com.depth.learningcrew.domain.auth.dto.AuthDto;
 import com.depth.learningcrew.domain.auth.service.AuthService;
 import com.depth.learningcrew.domain.user.dto.UserDto;
 import com.depth.learningcrew.system.security.model.JwtDto;
+import com.depth.learningcrew.system.security.model.UserDetails;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -44,5 +47,15 @@ public class AuthController {
     @ApiResponse(responseCode = "200", description = "토큰 재발행 성공")
     public JwtDto.TokenInfo refreshToken(@RequestBody @Valid AuthDto.RecreateRequest request) {
         return authService.recreateToken(request);
+    }
+
+    @GetMapping("/me")
+    @Operation(summary = "내 정보 조회", description = "현재 로그인된 사용자의 정보를 조회합니다.")
+    @ApiResponse(responseCode = "200", description = "내 정보 조회 성공")
+    public UserDto.UserResponse whoami(
+            @Parameter(hidden = true)
+            @AuthenticationPrincipal UserDetails userDetails
+    ) {
+        return UserDto.UserResponse.from(userDetails.getUser());
     }
 }
