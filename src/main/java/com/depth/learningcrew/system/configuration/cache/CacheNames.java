1bef1a2f7ed27b13c249f27dc7eb10d75cee471b
Hooby <hooby11105@naver.com>
2025-07-21 17:47:02 +0900

    feat: CacheNames 구현->Cache Key로 쓸 CacheName 저장소

---
+++ src/main/java/com/depth/learningcrew/system/configuration/cache/CacheNames.java
@@ -0,0 +1,7 @@
+package com.depth.learningcrew.system.configuration.cache;
+
+public final class CacheNames {
+    private CacheNames() {}
+
+    public static final String REFRESH_TOKEN_UUID = "refreshTokenUuid";
+}
