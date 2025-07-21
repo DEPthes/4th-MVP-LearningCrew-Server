2bbd38141acfa74f3d79203d0b778c268a56460b
Hooby <hooby11105@naver.com>
2025-07-22 06:09:27 +0900

    feat: 회원가입 요청 DTO 정의 및 기존 DTO 일부 Annotation, Field 제거

--- src/main/java/com/depth/learningcrew/domain/auth/dto/AuthDto.java
+++ src/main/java/com/depth/learningcrew/domain/auth/dto/AuthDto.java
@@ -1,8 +1,13 @@
 package com.depth.learningcrew.domain.auth.dto;
 
+import com.depth.learningcrew.domain.user.entity.Gender;
+import com.depth.learningcrew.domain.user.entity.User;
 import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.*;
 import lombok.*;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 
 public class AuthDto {
@@ -11,11 +16,8 @@ public class AuthDto {
     @AllArgsConstructor
     @NoArgsConstructor
     @Builder
-    @Getter
     @Schema(description = "토큰 재발행 DTO")
     public static class RecreateRequest {
-        @Schema(description = "확인할 ID", example = "dev1234")
-        private String id;
         @Schema(description = "재발행할 Refresh Token", example = "refreshTokenString")
         private String refreshToken;
     }
@@ -24,7 +26,6 @@ public class AuthDto {
     @AllArgsConstructor
     @NoArgsConstructor
     @Builder
-    @Getter
     @Schema(description = "토큰 정보 DTO")
     public static class TokenInfo {
         @Schema(description = "발급된 Access Token")
@@ -50,6 +51,52 @@ public class AuthDto {
                     .refreshToken(refreshToken)
                     .accessTokenExpiresAt(accessTokenExpiresAt)
                     .refreshTokenExpiresAt(refreshTokenExpiresAt)
+                    .build();
+        }
+    }
+
+    // 회원가입 요청 DTO
+    @Data
+    @AllArgsConstructor
+    @NoArgsConstructor
+    @Builder
+    @Schema(description = "회원가입 요청 DTO")
+    public static class SignUpRequest {
+        @NotBlank(message = "이메일을 입력해주세요.")
+        @Email(message = "올바른 이메일 형식이 아닙니다.")
+        @Schema(description = "사용자 아이디(이메일 형식)", example = "learnit@mju.ac.kr")
+        private String id;
+
+        @NotBlank(message = "특수문자를 제외한 2~10자리의 닉네임을 입력해주세요.")
+        @Pattern(regexp = "^[ㄱ-ㅎ가-힣a-zA-Z0-9-_]{2,10}$", message = "닉네임 조건에 충족되지 않습니다.")
+        @Schema(description = "사용자 닉네임", example = "LearnIt팀1")
+        private String nickname;
+
+        @NotBlank(message = "대소문자 영문자와 숫자를 포함한 8자리 이상의 비밀번호를 입력해주세요.")
+        @Pattern(regexp = "(?=.*[0-9])(?=.*[a-zA-Z]).{8,}", message = "비밀번호 조건에 충족되지 않습니다.")
+        @Schema(description = "사용자 비밀번호", example = "newpassword123 || NewPassword123")
+        private String password;
+
+        @NotNull(message = "생년월일을 입력해주세요.")
+        @Schema(description = "사용자 생년월일", example = "2000-01-01")
+        private LocalDate birthday;
+
+        @NotNull(message = "성별을 입력해주세요.")
+        @Schema(
+                description = "사용자 성별",
+                example = "MALE",
+                allowableValues = {"MALE", "FEMALE", "OTHER"}
+        )
+        private Gender gender;
+
+
+        public User toEntity(PasswordEncoder encoder) {
+            return User.builder()
+                    .id(id)
+                    .nickname(nickname)
+                    .password(encoder.encode(password))
+                    .birthday(birthday)
+                    .gender(gender)
                     .build();
         }
     }
