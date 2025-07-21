c6260cc4183fc759db7cb8fb6beecd322873bf01
Hooby <hooby11105@naver.com>
2025-07-21 18:40:35 +0900

    refactor: UserDetails에서 getUser를 할 수 있도록 AuthDetails에서 추상메서드 getUser 정의

--- src/main/java/com/depth/learningcrew/system/security/model/AuthDetails.java
+++ src/main/java/com/depth/learningcrew/system/security/model/AuthDetails.java
@@ -1,5 +1,6 @@
 package com.depth.learningcrew.system.security.model;
 
+import com.depth.learningcrew.domain.user.entity.User;
 import org.springframework.security.core.AuthenticatedPrincipal;
 import org.springframework.security.core.GrantedAuthority;
 
@@ -11,6 +12,8 @@ public abstract class AuthDetails implements AuthenticatedPrincipal {
     public String getName() {
         return this.getKey();
     }
+
+    public abstract User getUser();
 
     public abstract Collection<? extends GrantedAuthority> getAuthorities();
 }
