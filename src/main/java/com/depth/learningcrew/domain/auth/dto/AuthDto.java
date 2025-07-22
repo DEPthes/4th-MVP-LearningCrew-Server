b23dc201304328351d2127de35398b3dcf8e028d
Hooby <hooby11105@naver.com>
2025-07-22 09:16:25 +0900

    Docs: AuthDto에서 Swagger 문구 간단한 수정

--- src/main/java/com/depth/learningcrew/domain/auth/dto/AuthDto.java
+++ src/main/java/com/depth/learningcrew/domain/auth/dto/AuthDto.java
@@ -21,7 +21,7 @@ public class AuthDto {
     @Builder
     @Schema(description = "회원가입 요청 DTO")
     public static class SignUpRequest {
-        @NotBlank(message = "이메일을 입력해주세요.")
+        @NotBlank(message = "아이디를 이메일 형식으로 입력해주세요.")
         @Email(message = "올바른 이메일 형식이 아닙니다.")
         @Schema(description = "사용자 아이디(이메일 형식)", example = "learnit@mju.ac.kr")
         private String id;
