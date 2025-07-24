631e8aa79e28aad9e1f0116013c35d94cb7649fe
Hooby <hooby11105@naver.com>
2025-07-24 13:13:27 +0900

    refactor: User->pk구조,nickname 설정,update메소드 추가

--- src/main/java/com/depth/learningcrew/domain/user/entity/User.java
+++ src/main/java/com/depth/learningcrew/domain/user/entity/User.java
@@ -1,9 +1,11 @@
 package com.depth.learningcrew.domain.user.entity;
 
 import com.depth.learningcrew.common.auditor.TimeStampedEntity;
+import com.depth.learningcrew.domain.user.dto.UserDto;
 import jakarta.persistence.*;
 import lombok.*;
 import lombok.experimental.SuperBuilder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 import java.time.LocalDate;
 
@@ -15,18 +17,22 @@ import java.time.LocalDate;
 @Table(
     name = "USER_ACCOUNT",
     uniqueConstraints = {
-        @UniqueConstraint(name = "USER_NICKNAME", columnNames = "nickname")
+        @UniqueConstraint(name = "USER_NICKNAME", columnNames = "nickname"),
+        @UniqueConstraint(name = "USER_EMAIL", columnNames = "email")
     }
 )
 public class User extends TimeStampedEntity {
-    @Id
-    @Column(length = 50)
-    private String id;
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(nullable=false, length = 50)
+    private String email;
 
     @Column(nullable=false, length = 60)
     private String password;
 
-    @Column(nullable=false, length = 30)
+    // 대소문자 구분해서 저장하도록 COLLATE 설정 (단, 이는 MySQL, MariaDB에서 가능)(ex. Hooby <-> hooby)
+    @Column(nullable=false, length = 30, columnDefinition = "VARCHAR(30) COLLATE utf8mb4_bin")
     private String nickname;
 
     @Column(nullable=false)
@@ -42,11 +48,11 @@ public class User extends TimeStampedEntity {
 
     // TODO: AttachedFile Entity 구현 이후 연관관계 매핑 적용
 
-    public void updateNickname(String nickname) {
-        this.nickname = nickname;
-    }
+    public void update(UserDto.UserUpdateRequest request, PasswordEncoder encoder) {
+        if (!this.email.equals(request.getEmail())) {this.email = request.getEmail();}
 
-    public void updatePassword(String encodedPassword) {
-        this.password = encodedPassword;
+        if (!this.nickname.equals(request.getNickname())) {this.nickname = request.getNickname();}
+
+        this.password = encoder.encode(request.getPassword());
     }
 }
