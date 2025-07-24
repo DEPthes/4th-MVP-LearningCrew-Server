a9e766dd46c0c51c967e45c3d40b6551a41be9fd
HuniHuns <i690367@gmail.com>
2025-07-24 21:21:04 +0900

    feat: FileHandler 구현

---
+++ src/main/java/com/depth/learningcrew/domain/file/handler/FileHandler.java
@@ -0,0 +1,60 @@
+package com.depth.learningcrew.domain.file.handler;
+
+import com.depth.learningcrew.domain.file.entity.AttachedFile;
+import com.depth.learningcrew.system.exception.model.ErrorCode;
+import com.depth.learningcrew.system.exception.model.RestException;
+import lombok.RequiredArgsConstructor;
+import lombok.SneakyThrows;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.core.io.Resource;
+import org.springframework.stereotype.Component;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.nio.file.Paths;
+
+@Component
+@RequiredArgsConstructor
+public class FileHandler {
+
+    @Value("${file.save-path}")
+    private String savePath;
+
+    @SneakyThrows
+    public void saveFile(MultipartFile multipartFile, AttachedFile attachedFile) {
+        createDirIfNotExist(savePath);
+
+        File targetFile = Paths.get(savePath, attachedFile.getUuid()).toFile();
+
+        if(targetFile.exists()){
+            throw new RestException(ErrorCode.FILE_ALREADY_EXISTS);
+        }
+
+        try (FileOutputStream fileOutputStream = new FileOutputStream(targetFile)) {
+            fileOutputStream.write(multipartFile.getBytes());
+            fileOutputStream.flush();
+        } catch (Exception e) {
+            throw new RestException(ErrorCode.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    private void createDirIfNotExist(String path) {
+        File targetDir = Paths.get(path).toFile();
+        if(!targetDir.exists()) {
+            targetDir.mkdirs();
+        }
+    }
+
+    @SneakyThrows
+    public Resource loadFileAsResource(AttachedFile attachedFile) {
+        File targetFile = Paths.get(savePath, attachedFile.getUuid()).toFile();
+
+        if(!targetFile.exists()){
+            throw new RestException(ErrorCode.FILE_NOT_FOUND);
+        }
+
+        return new FileSystemResource(targetFile);
+    }
+}
