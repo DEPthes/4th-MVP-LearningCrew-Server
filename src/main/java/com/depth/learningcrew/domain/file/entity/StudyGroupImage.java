4e752e536ec507167a86249a28889ff0e0711d44
ksy132336 <ksygiru@gmail.com>
2025-07-26 01:36:17 +0900

    feat: StudyGroupImage Entity 생성

---
+++ src/main/java/com/depth/learningcrew/domain/file/entity/StudyGroupImage.java
@@ -0,0 +1,54 @@
+package com.depth.learningcrew.domain.file.entity;
+
+import com.depth.learningcrew.domain.studygroup.entity.StudyGroup;
+import com.depth.learningcrew.system.exception.model.ErrorCode;
+import com.depth.learningcrew.system.exception.model.RestException;
+import jakarta.persistence.*;
+import lombok.*;
+import lombok.experimental.SuperBuilder;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
+import java.util.UUID;
+
+@Entity
+@Getter
+@Setter
+@SuperBuilder
+@NoArgsConstructor
+public class StudyGroupImage extends AttachedFile {
+
+    @OneToOne
+    private StudyGroup studyGroup;
+
+    public static StudyGroupImage from(MultipartFile file, StudyGroup studyGroup) {
+        throwIfNotAImageFile(file);
+
+        String filename = file.getOriginalFilename();
+        if (filename == null || filename.isBlank()) {
+            throw new RestException(ErrorCode.FILE_NOT_FOUND);
+        }
+
+        return StudyGroupImage.builder()
+                .uuid(UUID.randomUUID().toString())
+                .fileName(file.getOriginalFilename())
+                .handingType(HandingType.IMAGE)
+                .studyGroup(studyGroup)
+                .build();
+
+    }
+
+    private static void throwIfNotAImageFile(MultipartFile file) {
+        String fileName = file.getOriginalFilename();
+        String[] splitted = fileName.split("\\.");
+
+        if(splitted.length < 1) {
+            throw new RestException(ErrorCode.FILE_NOT_IMAGE);
+        }
+
+        String extension = splitted[splitted.length - 1];
+        if(!List.of("JPG").contains(extension.toUpperCase())) {
+            throw new RestException(ErrorCode.FILE_NOT_IMAGE);
+        }
+    }
+}
