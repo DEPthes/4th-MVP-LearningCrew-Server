30a4ed394b7227bdb638f49b91dc4d815b213c11
ksy132336 <ksygiru@gmail.com>
2025-07-25 23:39:59 +0900

    feat: Dibs Entity, DibsId 복합키 클래스 생성

---
+++ src/main/java/com/depth/learningcrew/domain/studygroup/entity/DibsId.java
@@ -0,0 +1,25 @@
+package com.depth.learningcrew.domain.studygroup.entity;
+
+import com.depth.learningcrew.domain.user.entity.User;
+import jakarta.persistence.Embeddable;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.*;
+
+@Embeddable
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode
+@Builder
+public class DibsId {
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "study_group_id", nullable = false)
+    private StudyGroup studyGroup;
+}
