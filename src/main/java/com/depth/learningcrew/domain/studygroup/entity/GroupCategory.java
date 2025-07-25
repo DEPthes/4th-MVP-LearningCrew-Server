3cda7560dad3cc82c6b80d7cfcdd4ace02f2afa5
ksy132336 <ksygiru@gmail.com>
2025-07-25 13:34:32 +0900

    feat: GroupCategory Entity 생성

---
+++ src/main/java/com/depth/learningcrew/domain/studygroup/entity/GroupCategory.java
@@ -0,0 +1,24 @@
+package com.depth.learningcrew.domain.studygroup.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class GroupCategory {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(length = 50, nullable = false, unique = true)
+    private String name;
+
+    @ManyToMany(mappedBy = "categories")
+    private List<StudyGroup> studyGroups = new ArrayList<>();
+}
