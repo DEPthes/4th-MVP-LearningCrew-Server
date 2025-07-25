a5516340563ea1308163b5b909c884d8e67e25ff
ksy132336 <ksygiru@gmail.com>
2025-07-25 23:28:19 +0900

    feat: Member Entity, MemberId 복합키 클래스 생성

---
+++ src/main/java/com/depth/learningcrew/domain/studygroup/entity/Member.java
@@ -0,0 +1,18 @@
+package com.depth.learningcrew.domain.studygroup.entity;
+
+import com.depth.learningcrew.common.auditor.TimeStampedEntity;
+import jakarta.persistence.EmbeddedId;
+import jakarta.persistence.Entity;
+import lombok.*;
+import lombok.experimental.SuperBuilder;
+
+@Entity
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@SuperBuilder
+public class Member extends TimeStampedEntity {
+
+    @EmbeddedId
+    private MemberId id;
+}
---
+++ src/main/java/com/depth/learningcrew/domain/studygroup/entity/MemberId.java
@@ -0,0 +1,25 @@
+package com.depth.learningcrew.domain.studygroup.entity;
+
+import com.depth.learningcrew.domain.user.entity.User;
+import jakarta.persistence.*;
+import jakarta.persistence.ManyToOne;
+import lombok.*;
+
+import java.io.Serializable;
+
+@Embeddable
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode
+@Builder
+public class MemberId implements Serializable {
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "study_group_id", nullable = false)
+    private StudyGroup studyGroup;
+}
