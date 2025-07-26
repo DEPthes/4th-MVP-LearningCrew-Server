c4aa8804d24f4f1770539919fb265f28ebe3b5ef
ksy132336 <ksygiru@gmail.com>
2025-07-26 17:10:30 +0900

    feat: ApplicationId에 정적 팩토리 메서드 of() 추가

--- src/main/java/com/depth/learningcrew/domain/studygroup/entity/ApplicationId.java
+++ src/main/java/com/depth/learningcrew/domain/studygroup/entity/ApplicationId.java
@@ -24,4 +24,11 @@ public class ApplicationId implements Serializable {
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "study_group_id", nullable = false)
     private StudyGroup studyGroup;
+
+    public static ApplicationId of(User user, StudyGroup studyGroup) {
+        return ApplicationId.builder()
+                .user(user)
+                .studyGroup(studyGroup)
+                .build();
+    }
 }
