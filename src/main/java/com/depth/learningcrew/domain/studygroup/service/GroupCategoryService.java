3884c0976187be984394a17de7f8e79f4ce68e64
HuniHuns <i690367@gmail.com>
2025-07-29 13:30:48 +0900

    feat: Category 조회 서비스 로직 작성

---
+++ src/main/java/com/depth/learningcrew/domain/studygroup/service/GroupCategoryService.java
@@ -0,0 +1,26 @@
+package com.depth.learningcrew.domain.studygroup.service;
+
+import com.depth.learningcrew.domain.studygroup.dto.GroupCategoryDto;
+import com.depth.learningcrew.domain.studygroup.entity.GroupCategory;
+import com.depth.learningcrew.domain.studygroup.repository.GroupCategoryRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+@RequiredArgsConstructor
+public class GroupCategoryService {
+
+    private final GroupCategoryRepository groupCategoryRepository;
+
+    @Transactional(readOnly = true)
+    public List<GroupCategoryDto.GroupCategoryResponse> getGroupCategory() {
+        List<GroupCategory> groupCategories = groupCategoryRepository.findAll();
+        return groupCategories.stream()
+                .map(GroupCategoryDto.GroupCategoryResponse::from)
+                .collect(Collectors.toList());
+    }
+}
