42074ec564755f886ad13462b5c914efa0fe4d9e
ksy132336 <ksygiru@gmail.com>
2025-08-01 04:47:38 +0900

    feat: DibsRepository에 existsByIdUserAndIdStudyGroup 메서드 추가

--- src/main/java/com/depth/learningcrew/domain/studygroup/repository/DibsRepository.java
+++ src/main/java/com/depth/learningcrew/domain/studygroup/repository/DibsRepository.java
@@ -2,7 +2,11 @@ package com.depth.learningcrew.domain.studygroup.repository;
 
 import com.depth.learningcrew.domain.studygroup.entity.Dibs;
 import com.depth.learningcrew.domain.studygroup.entity.DibsId;
+import com.depth.learningcrew.domain.studygroup.entity.StudyGroup;
+import com.depth.learningcrew.domain.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface DibsRepository extends JpaRepository<Dibs, DibsId> {
+
+    boolean existsByIdUserAndIdStudyGroup(User user, StudyGroup studyGroup);
 }
