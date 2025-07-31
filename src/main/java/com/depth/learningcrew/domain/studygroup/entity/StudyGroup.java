7a7898ec9862440b9d3eee4174f12260b1324cb1
ksy132336 <ksygiru@gmail.com>
2025-08-01 01:12:17 +0900

    feat: StudyGroup에 StudyStep 연관관계 필드 steps 추가

--- src/main/java/com/depth/learningcrew/domain/studygroup/entity/StudyGroup.java
+++ src/main/java/com/depth/learningcrew/domain/studygroup/entity/StudyGroup.java
@@ -1,24 +1,14 @@
 package com.depth.learningcrew.domain.studygroup.entity;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.List;
 
 import com.depth.learningcrew.common.auditor.TimeStampedEntity;
 import com.depth.learningcrew.domain.file.entity.StudyGroupImage;
 import com.depth.learningcrew.domain.user.entity.User;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.JoinTable;
-import jakarta.persistence.Lob;
-import jakarta.persistence.ManyToMany;
-import jakarta.persistence.ManyToOne;
-import jakarta.persistence.OneToOne;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -70,4 +60,7 @@ public class StudyGroup extends TimeStampedEntity {
 
     @OneToOne(mappedBy = "studyGroup")
     private StudyGroupImage studyGroupImage;
+
+    @OneToMany(mappedBy = "id.studyGroupId")
+    private List<StudyStep> steps = new ArrayList<>();
 }
