1ff4693fd979f50e8189bcea6ab7d7560df22b47
Hooby <hooby11105@naver.com>
2025-08-05 12:52:14 +0900

    comment: 업데이트 부분 의문점 코멘트 정리

--- src/main/java/com/depth/learningcrew/domain/user/service/UserService.java
+++ src/main/java/com/depth/learningcrew/domain/user/service/UserService.java
@@ -42,10 +42,10 @@ public class UserService {
 
         request.applyTo(found, passwordEncoder);
 
-        /* TODO: 로직 점검 (참새가 봉황의 뜻을 헤아릴 수 없기에 코드를 함부로 건드리지 않고 이렇게 남김)
-        * 1. found에 setProfileImage를 하는데, user에도 setProfileImage를 하는 이유가 궁금
-        * 2. ProfileImage가 연관관계 주인이면, profileImage.setUser(found) 가 있어야 하지 않나요?
-        * 3. profileImageRepository.save(profileImage); 필요하지 않나요?
+        /* TODO: 로직 점검
+        * 1. found에 setProfileImage를 하는데, user에도 setProfileImage를 하는 이유가 궁금합니다
+        * 2. ProfileImage가 연관관계 주인이면, profileImage.setUser(found) 가 있어야 하지 않은지 의문입니다
+        * 3. profileImageRepository.save(profileImage); 필요하지 않나하는 생각이 듭니다
         *    PersistenceContext에 반영되지 않아서 profileImage가 영속화되지 않을 것 같아요.
         * */
         if (request.getProfileImage() != null) {
@@ -54,15 +54,15 @@ public class UserService {
             }
 
             ProfileImage profileImage = ProfileImage.from(request.getProfileImage());
-            // Suggest1. 연관관계 주인이면, profileImage.setUser(found) 가 있어야할 듯
+            // 생각1. 연관관계 주인이면, profileImage.setUser(found) 가 있어야할 것 같아요
             // profileImage.setUser(found);
 
-            // Suggest2. profileImageRepository.save(profileImage); 필요할 것 같음
+            // 생각2. profileImageRepository.save(profileImage); 필요할 것 같아요
             // profileImageRepository.save(profileImage);
 
             fileHandler.saveFile(request.getProfileImage(), profileImage);
             found.setProfileImage(profileImage);
-            user.setProfileImage(profileImage); // Suggest3. 이건 제거해도 될 것 같음
+            user.setProfileImage(profileImage); // 생각3. 이건 제거해도 되지 않을까 하는 생각이 듭니다
         }
 
         return UserDto.UserUpdateResponse.from(found);
