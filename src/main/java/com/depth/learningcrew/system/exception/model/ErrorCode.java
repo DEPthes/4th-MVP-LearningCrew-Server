6c3e8b5ef4d6744a41505aff6acd396d75672914
ksy132336 <ksygiru@gmail.com>
2025-08-09 02:49:10 +0900

    feat: 현재 스텝이 아닌경우의 에러코드 추가

--- src/main/java/com/depth/learningcrew/system/exception/model/ErrorCode.java
+++ src/main/java/com/depth/learningcrew/system/exception/model/ErrorCode.java
@@ -60,6 +60,7 @@ public enum ErrorCode {
     STUDY_GROUP_OWNER_CANNOT_BE_EXPELLED(400, "스터디 그룹의 소유자는 추방할 수 없습니다."),
     STUDY_GROUP_NOT_CURRENT_STEP(400, "현재 스텝에서는 질문을 생성할 수 없습니다."),
     STUDY_GROUP_NOT_MEMBER(400, "스터디 그룹의 멤버가 아닙니다."),
+    STUDY_GROUP_STEP_NOT_WRITABLE(400, "현재 진행 중인 스텝에서만 작성할 수 있습니다."),
 
     // Group Category
     GROUP_CATEGORY_ALREADY_EXIST(400, "중복되는 카테고리명입니다."),
