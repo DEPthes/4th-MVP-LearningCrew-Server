a19e227b9a5472c705fe21e3b198e94fd043b6cb
ksy132336 <ksygiru@gmail.com>
2025-08-11 02:31:55 +0900

    feat: NoteRepository에 중복 노트 존재 여부 검사 메서드 추가

--- src/main/java/com/depth/learningcrew/domain/note/repository/NoteRepository.java
+++ src/main/java/com/depth/learningcrew/domain/note/repository/NoteRepository.java
@@ -4,4 +4,5 @@ import com.depth.learningcrew.domain.note.entity.Note;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface NoteRepository extends JpaRepository<Note, Long> {
+    boolean existsByStudyGroup_IdAndStepAndCreatedBy_Id(Long studyGroupId, Integer step, Long userId);
 }
