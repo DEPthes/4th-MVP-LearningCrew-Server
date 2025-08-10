5282bc96267091f4032af7e6d8aafb69df46c5e1
Hooby <hooby11105@naver.com>
2025-08-11 04:11:47 +0900

    feat: studyStep에 대해 @JoinColumns로 StudyStep(복합 FK) 매핑 추가

--- src/main/java/com/depth/learningcrew/domain/quiz/entity/Quiz.java
+++ src/main/java/com/depth/learningcrew/domain/quiz/entity/Quiz.java
@@ -7,18 +7,7 @@ import com.depth.learningcrew.common.auditor.TimeStampedEntity;
 import com.depth.learningcrew.domain.studygroup.entity.StudyGroup;
 import com.depth.learningcrew.domain.studygroup.entity.StudyStep;
 
-import jakarta.persistence.CascadeType;
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.Lob;
-import jakarta.persistence.ManyToOne;
-import jakarta.persistence.OneToMany;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -53,6 +42,10 @@ public class Quiz extends TimeStampedEntity {
   private StudyGroup studyGroup;
 
   @ManyToOne(fetch = FetchType.LAZY)
+  @JoinColumns({
+          @JoinColumn(name = "step", referencedColumnName = "step", insertable = false, updatable = false),
+          @JoinColumn(name = "study_group_id", referencedColumnName = "study_group_id", insertable = false, updatable = false)
+  })
   private StudyStep studyStep;
 
   @OneToMany(mappedBy = "id.quiz", cascade = CascadeType.ALL, orphanRemoval = true)
