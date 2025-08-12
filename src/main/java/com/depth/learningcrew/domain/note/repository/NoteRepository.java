bc265a5aea8814c17a8d5edb1693747dffc2b041
ksy132336 <ksygiru@gmail.com>
2025-08-12 23:56:30 +0900

    feat: NoteRepository에  노트 단건 조회 메서드 추가

--- src/main/java/com/depth/learningcrew/domain/note/repository/NoteRepository.java
+++ src/main/java/com/depth/learningcrew/domain/note/repository/NoteRepository.java
@@ -4,6 +4,7 @@ import com.depth.learningcrew.domain.note.entity.Note;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface NoteRepository extends JpaRepository<Note, Long> {
 
@@ -11,4 +12,5 @@ public interface NoteRepository extends JpaRepository<Note, Long> {
 
     boolean existsByStudyGroup_IdAndStepAndCreatedBy_Id(Long studyGroupId, Integer step, Long userId);
 
+    Optional<Note> findByStudyGroup_IdAndStepAndCreatedBy_Id(Long studyGroupId, Integer step, Long userId);
 }
